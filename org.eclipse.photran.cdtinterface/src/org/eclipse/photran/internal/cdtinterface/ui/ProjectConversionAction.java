d3c8dc5e11383c36c4dfea6393a6b7f06784acc2
Jeffrey Overbey <jeffreyoverbey@acm.org>
2011-11-04 14:57:51 -0500

    Bug 349355 - Replace Fortran View with Project Explorer

--- org.eclipse.photran.cdtinterface/src/org/eclipse/photran/internal/cdtinterface/ui/ProjectConversionAction.java
+++ org.eclipse.photran.cdtinterface/src/org/eclipse/photran/internal/cdtinterface/ui/ProjectConversionAction.java
@@ -1,16 +1,20 @@
 /*******************************************************************************
- * Copyright (c) 2009 Eclipse Engineering LLC.
- *
+ * Copyright (c) 2009, 2011 Eclipse Engineering LLC and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *   Eclipse Engineering LLC (Matt Scarpino) - Initial API and implementation
+ *   University of Illinois (Jeff Overbey) - Updated for Projects View
  *******************************************************************************/
 package org.eclipse.photran.internal.cdtinterface.ui;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 
+import org.eclipse.cdt.core.CProjectNature;
 import org.eclipse.cdt.core.model.ICProject;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IProjectDescription;
@@ -20,26 +24,30 @@ import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.photran.internal.cdtinterface.CDTInterfacePlugin;
 import org.eclipse.photran.internal.core.FProjectNature;
+import org.eclipse.photran.internal.core.FortranCorePlugin;
 import org.eclipse.ui.IViewActionDelegate;
 import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.navigator.CommonNavigator;
 
 /**
- * Implements the "Convert to Fortran Project" action, which can be invoked by right-clicking on
- * a C/C++ Project in the Fortran Projects view.
+ * Implements the "Convert to Fortran Project" action, which can be invoked by right-clicking on a
+ * C/C++ Project in the Fortran Projects view.
  * 
  * @author Matt Scarpino
+ * @author Jeff Overbey
  */
 public class ProjectConversionAction implements IViewActionDelegate
 {
-    private FortranView view;
-    private ArrayList<IProject> projects;
+    private IViewPart view = null;
+
+    private ArrayList<IProject> projects = new ArrayList<IProject>();
 
     /**
      * Callback invoked to initialize this action.
      */
     public void init(IViewPart v)
     {
-        view = (FortranView)v;
+        view = v;
         projects = new ArrayList<IProject>();
     }
 
@@ -53,13 +61,30 @@ public class ProjectConversionAction implements IViewActionDelegate
     {
         if (selection instanceof IStructuredSelection)
         {
-            IStructuredSelection structuredSelection = (IStructuredSelection) selection;
+            IStructuredSelection structuredSelection = (IStructuredSelection)selection;
             Iterator e = structuredSelection.iterator();
             while (e.hasNext())
             {
                 Object element = e.next();
-                if (element instanceof ICProject)
+                if (element instanceof IProject)
+                {
+                    try
+                    {
+                        IProject project = (IProject)element;
+                        if (project.hasNature(CProjectNature.C_NATURE_ID))
+                        {
+                            projects.add(project);
+                        }
+                    }
+                    catch (CoreException ex)
+                    {
+                        FortranCorePlugin.log(ex);
+                    }
+                }
+                else if (element instanceof ICProject)
+                {
                     projects.add(((ICProject)element).getProject());
+                }
             }
         }
     }
@@ -70,13 +95,12 @@ public class ProjectConversionAction implements IViewActionDelegate
      * Adds the Fortran nature to selected C/C++ projects, and refreshes the view to display the new
      * nature image (i.e., to make sure the project is displayed with an &quot;F&quot; icon).
      */
-    @SuppressWarnings("restriction")
     public void run(IAction action)
     {
         if (!projects.isEmpty())
         {
             addFortranNatureToSelectedProjects();
-            view.getViewer().refresh();
+            refreshView();
             projects.clear();
         }
     }
@@ -84,7 +108,9 @@ public class ProjectConversionAction implements IViewActionDelegate
     private void addFortranNatureToSelectedProjects()
     {
         for (IProject project : projects)
+        {
             addFortranNatureTo(project);
+        }
     }
 
     private void addFortranNatureTo(IProject project)
@@ -106,6 +132,19 @@ public class ProjectConversionAction implements IViewActionDelegate
         {
             CDTInterfacePlugin.log(e);
             e.printStackTrace();
+        }
+    }
+
+    @SuppressWarnings("restriction")
+    private void refreshView()
+    {
+        if (view instanceof FortranView)
+        {
+            ((FortranView)view).getViewer().refresh();
+        }
+        else if (view instanceof CommonNavigator)
+        {
+            ((CommonNavigator)view).getCommonViewer().refresh();
         }
     }
 }
