57a73099effb0f4890cf60f3f9e4bc342b84ed82
Jeffrey Overbey <jeffreyoverbey@acm.org>
2014-07-16 23:31:14 -0400

    Deleted VPG extension point; hard-coded PhotranVPG

--- org.eclipse.photran.ui.vpg/rephraser/org/eclipse/rephraserengine/ui/actions/VPGWindowActionDelegate.java
+++ org.eclipse.photran.ui.vpg/rephraser/org/eclipse/rephraserengine/ui/actions/VPGWindowActionDelegate.java
@@ -11,32 +11,22 @@
 package org.eclipse.rephraserengine.ui.actions;
 
 import java.lang.reflect.InvocationTargetException;
-import java.util.Map;
-import java.util.TreeMap;
-import java.util.Map.Entry;
 
 import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.jobs.ISchedulingRule;
 import org.eclipse.core.runtime.jobs.MultiRule;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.IStructuredContentProvider;
-import org.eclipse.jface.viewers.LabelProvider;
-import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.photran.internal.core.vpg.PhotranVPG;
 import org.eclipse.rephraserengine.core.vpg.eclipse.EclipseVPG;
 import org.eclipse.rephraserengine.core.vpg.eclipse.VPGSchedulingRule;
-import org.eclipse.rephraserengine.ui.IEclipseVPGFactory;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.dialogs.ListDialog;
 import org.eclipse.ui.progress.IProgressService;
 
 /**
@@ -58,8 +48,6 @@ public abstract class VPGWindowActionDelegate
            implements IWorkbenchWindowActionDelegate,
                       IRunnableWithProgress
 {
-    private static final String VPG_EXTENSION_POINT_ID = "org.eclipse.photran.ui.vpg.internal.vpg"; //$NON-NLS-1$
-
     private EclipseVPG vpg = null;
 
     /** The active workbench window; may be <code>null</code> */
@@ -80,19 +68,8 @@ public abstract class VPGWindowActionDelegate
 
     public final void run(IAction action)
     {
-        vpg = determineVPG();
-
-        if (vpg == null)
-        {
-            MessageDialog.openError(
-                    activeShell,
-                    Messages.VPGWindowActionDelegate_ErrorTitle,
-                    Messages.VPGWindowActionDelegate_NoVPGsAvailable);
-        }
-        else
-        {
-            scheduleThisUsingVPGSchedulingRule();
-        }
+        vpg = PhotranVPG.getInstance();
+        scheduleThisUsingVPGSchedulingRule();
     }
 
     private void scheduleThisUsingVPGSchedulingRule()
@@ -118,96 +95,6 @@ public abstract class VPGWindowActionDelegate
         catch (InterruptedException e)
         {
             // Do nothing
-        }
-    }
-
-    private EclipseVPG determineVPG()
-    {
-        IConfigurationElement[] configs = Platform.getExtensionRegistry().getConfigurationElementsFor(VPG_EXTENSION_POINT_ID);
-        if (configs.length == 0)
-            return null;
-        else
-            return createVPG(configs, determineWhichVPGToUse(configs));
-    }
-
-    private EclipseVPG createVPG(IConfigurationElement[] configs, int index)
-    {
-        try
-        {
-            if (index < 0)
-            {
-                return null;
-            }
-            else
-            {
-//                MessageDialog.openInformation(
-//                        activeShell,
-//                        "FYI",
-//                        "You chose " + configs[index].getAttribute("name"));
-
-                IEclipseVPGFactory factory = (IEclipseVPGFactory)configs[index].createExecutableExtension("class"); //$NON-NLS-1$
-                return factory.getVPG();
-            }
-        }
-        catch (CoreException e)
-        {
-            return null;
-        }
-    }
-
-    private int determineWhichVPGToUse(IConfigurationElement[] configs)
-    {
-        if (configs.length == 1)
-            return 0;
-        else
-            return askUserWhichVPGToUse(configs);
-    }
-
-    @SuppressWarnings("unchecked")
-    private int askUserWhichVPGToUse(IConfigurationElement[] configs)
-    {
-        Map<Integer, String> vpgs = new TreeMap<Integer, String>();
-        for (int i = 0; i < configs.length; i++)
-            vpgs.put(i, configs[i].getAttribute("name")); //$NON-NLS-1$
-
-        ListDialog dlg = new ListDialog(activeShell);
-        dlg.setInput(vpgs);
-        dlg.setTitle(Messages.VPGWindowActionDelegate_SelectVPGTitle);
-        dlg.setContentProvider(new ArrayPairContentProvider());
-        dlg.setLabelProvider(new ArrayPairLabelProvider());
-        dlg.setMessage(Messages.VPGWindowActionDelegate_SelectDatabaseToUse);
-        dlg.setBlockOnOpen(true);
-        if (dlg.open() == ListDialog.OK
-                && dlg.getResult() != null
-                && dlg.getResult().length > 0)
-        {
-            return ((Entry<Integer, String>)dlg.getResult()[0]).getKey();
-        }
-        else
-        {
-            return -1;
-        }
-    }
-
-    private static final class ArrayPairContentProvider implements IStructuredContentProvider
-    {
-        @SuppressWarnings("unchecked")
-        public Object[] getElements(Object inputElement)
-        {
-            return ((Map<Integer, String>)inputElement).entrySet().toArray();
-        }
-
-        public void dispose() {;}
-        public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {;}
-    }
-
-    private static final class ArrayPairLabelProvider extends LabelProvider
-    {
-        @SuppressWarnings("unchecked")
-        @Override
-        public String getText(Object element)
-        {
-            return ((Entry<Integer, String>)element).getValue();
         }
     }
 
